82a80af11332e1a49ed857d7edaa13576c3dc3e7
specatron111 <61760656+specatron111@users.noreply.github.com>
2020-06-28 00:48:45 +0100

    i fucked up

--- dark.js
+++ dark.js
@@ -1,9 +1,14 @@
-
-function setDarkMode(x) {
+var darkMode = localStorage.getItem('darkMode');
+console.log(darkMode);
+if (darkMode === null) {
+    darkMode = true
+}
+function setDarkMode() {
       for (let styleSheet of document.styleSheets) {
             if (styleSheet.href && styleSheet.href.includes('/light-mode.css')) {
-                  styleSheet.disabled = !x;
+                  styleSheet.disabled = !darkMode;
                   break;
             }
       }
 }
+$().ready(setDarkMode);
