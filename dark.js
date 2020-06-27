f4b9405cdb7203ca18111e5774b096c000f6fa99
specatron111 <61760656+specatron111@users.noreply.github.com>
2020-06-28 00:38:55 +0100

    Update dark.js

--- dark.js
+++ dark.js
@@ -1,8 +1,8 @@
 var darkMode = localStorage.getItem('darkMode');
+console.log(darkMode);
 if (darkMode === null) {
     darkMode = true
 }
-console.log(darkMode);
 function setDarkMode() {
       for (let styleSheet of document.styleSheets) {
             if (styleSheet.href && styleSheet.href.includes('/light-mode.css')) {
