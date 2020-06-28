44554bec181dabe76e39ab6afff06a8595f4dc39
Josh Heng <josh@joshheng.co.uk>
2020-06-28 13:38:16 +0100

    Fix dark mode
    Update dark.js & html pages

--- dark.js
+++ dark.js
@@ -1,13 +1,21 @@
 var darkMode = localStorage.getItem('darkMode');
-console.log(darkMode);
-if (darkMode === null) {
-    darkMode = true
-}
+if (darkMode === 'false') darkMode = false;
+else if (darkMode === 'true') darkMode = true;
+else darkMode = true;
+document.getElementById('darkmode-toggle').checked = !darkMode;
+
 function setDarkMode() {
-      for (let styleSheet of document.styleSheets) {
-            if (styleSheet.href && styleSheet.href.includes('/light-mode.css')) {
-                  styleSheet.disabled = !darkMode;
-                  break;
-            }
-      }
+	for (let styleSheet of document.styleSheets) {
+		if (styleSheet.href && styleSheet.href.includes('/light-mode.css')) {
+			styleSheet.disabled = darkMode;
+			break;
+		}
+	}
+}
+setDarkMode();
+
+function darkModeClicked(checkbox) {
+	darkMode = !checkbox.checked;
+	localStorage.setItem('darkMode', darkMode);
+	setDarkMode();
 }
